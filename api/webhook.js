d86db8d365994fa432b0c0d74d5d316ca05f81de
Johan Farfan <johanfarfan25@gmail.com>
2026-02-03 11:56:10 -0500

    ajuses meta

--- api/webhook.js
+++ api/webhook.js
@@ -1,25 +1,7 @@
 export default function handler(req, res) {
-  if (req.method === 'GET') {
-    const verifyToken = process.env.VERIFY_TOKEN;
-
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-
-    if (mode === 'subscribe' && token === verifyToken) {
-      console.log('WEBHOOK VERIFIED');
-      return res.status(200).send(challenge);
-    }
-
-    return res.status(403).send('Forbidden');
-  }
-
-  if (req.method === 'POST') {
-    console.log('Webhook received');
-    console.log(JSON.stringify(req.body, null, 2));
-
-    return res.status(200).json({ status: 'ok' });
-  }
-
-  return res.status(405).send('Method Not Allowed');
+  res.status(200).json({
+    ok: true,
+    message: 'Webhook vivo',
+    method: req.method
+  });
 }
